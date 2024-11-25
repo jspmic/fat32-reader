7ff803cc63d5c56b7022dc4cf42b01e93995e739
Jaspe Michael <micaeljaspe@gmail.com>
2024-11-25 20:19:40 +0200

    feat: Added basic FAT32 filesystem descriptor

--- src/header.h
+++ src/header.h
@@ -1,4 +1,12 @@
-enum FAT32{
-	FIRST_LAST_BYTE = 0x55,
-	SECOND_LAST_BYTE = 0xAA,
+enum FAT32_SECTOR{
+	BOOT_START				= 0, 		// The MBR beginning
+	BOOT_END				= 446, 		// The MBR ending
+	PARTITION_DESCRIPTION	= 16,
+	TYPE_CODE				= 5,		// The type code is the 5th byte of the description
+	LBA_BEGIN				= 9,
+	LBA_LAST				= 12,
+	FAT32_CODE				= 0x0B,
+	FAT32_CODE2				= 0x0C,
+	FIRST_LAST_BYTE			= 0x55,		// The first among the 2 last bytes of a sector
+	SECOND_LAST_BYTE		= 0xAA, 	// The very last byte of a sector
 };
--- src/reader.c
+++ src/reader.c
@@ -2,7 +2,24 @@
 #include <stdlib.h>
 #include "header.h"
 
+/*
+ * @description: This is a FAT32 disk reader, that uses Logical Block Addressing(LBA).
+ * @reference: For more info, read:
+	 * - https://www.pjrc.com/tech/8051/ide/fat32.html
+	 * - https://en.wikipedia.org/wiki/Design_of_the_FAT_file_system
+*/
+
+// Experimental disk image name(must be FAT32)
 #define DISK_NAME "fat32.img"
+
+// A sector is considered to be 512 bytes large
+#define SECTOR_SIZE 512
+
+// A partition is of size 16 bytes
+#define PARTITION_SIZE 16
+
+// FAT32 partition table is of size 64 bytes
+#define PARTITION_TABLE 64
 
 
 int main(int argc, char** argv){
