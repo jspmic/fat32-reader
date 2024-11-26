0aea79f7ca4653e125469fdc1a855f940939c671
jspmic <micaeljaspe@gmail.com>
2024-11-26 12:12:18 +0200

    fix: Placed fd to the end of the MBR and start reading partition descriptions

--- src/reader.c
+++ src/reader.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <assert.h>
 #include <unistd.h>
 #include "header.h"
 
@@ -27,8 +28,9 @@ int open_disk(const char* disk_name){
 		perror("read_disk");
 		exit(1);
 	}
+	// Place at the end of the MBR
 	if (lseek(fd, BOOT_END, 0) == -1){
-		perror("lseek - open_disk");
+		perror("open_disk");
 		exit(1);
 	}
 	return fd;
@@ -50,17 +52,15 @@ int read_disk(int fd, unsigned char buffer[], int start, int end){
 */
 
 // Read the n-th partition(note that only 4 partitions are available)
-void read_partition(int fd, unsigned char buffer[]){
-	int pos;
-	/* if (fd < BOOT_END) */
-	pos = read_disk(fd, buffer, 0, PARTITION_DESCRIPTION);
-	/* else */
-	/* 	pos = read_disk(fd, buffer, 0, PARTITION_DESCRIPTION); */
+void read_partition_description(int fd, unsigned char buffer[], int n){
+	assert(n > 0);
+	lseek(fd, n*PARTITION_DESCRIPTION, 0);
+	int pos = read_disk(fd, buffer, 0, PARTITION_DESCRIPTION);
 
-	if (pos == -1){
-		perror("read_partition");
+	if (pos == -1)
 		return;
-	}
+	if (lseek(fd, BOOT_END, 0) == -1)
+		perror("read_partition_description");
 }
 
 int main(int argc, char** argv){
@@ -70,7 +70,8 @@ int main(int argc, char** argv){
 	}
 	int fd = open_disk(argv[argc-1]);
 	unsigned char buffer[PARTITION_DESCRIPTION];
-	read_partition(fd, buffer);
+	/* read_partition_description(fd, buffer, 0); */
+	read_partition_description(fd, buffer, 1);
 	for (int i = 0; i<PARTITION_DESCRIPTION; i++)
 		printf("%x ", buffer[i]);
 	printf("\n");
