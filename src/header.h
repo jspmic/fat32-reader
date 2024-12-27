b2e5e27ccedb2b25b4aee4b96116c92418965806
Jaspe Michael <micaeljaspe@gmail.com>
2024-12-27 10:54:01 +0200

    fix: removed array uses to corresponding uints

--- src/header.h
+++ src/header.h
@@ -31,30 +31,30 @@ typedef struct{
 	uint8_t oem[OEM];
 
 	// BIOS fields
-	uint8_t bytesPerSector[WORD];
-	uint8_t sectorsPerCluster[BYTE];
-	uint8_t reservedSectors[WORD];
-	uint8_t numFat[BYTE];
-	uint8_t rootEntry[WORD];
-	uint8_t smallSectors[WORD];
-	uint8_t mediaType[BYTE];
-	uint8_t sectorsPerFat[WORD];
-	uint8_t sectorsPerTrack[WORD];
-	uint8_t numHeads[WORD];
-	uint8_t hiddenSectors[DWORD]; // DWORD = WORD*2
-	uint8_t largeSectors[DWORD];
-	uint8_t physicalDiskNumber[BYTE];
-	uint8_t currentHead[BYTE];
-	uint8_t signature[BYTE];
-	uint8_t volumeSerialNumber[VOLUMESER_NUM];
+	uint16_t bytesPerSector;
+	uint8_t sectorsPerCluster;
+	uint16_t reservedSectors;
+	uint8_t numFat;
+	uint16_t rootEntry;
+	uint16_t smallSectors;
+	uint8_t mediaType;
+	uint16_t sectorsPerFat;
+	uint16_t sectorsPerTrack;
+	uint16_t numHeads;
+	uint32_t hiddenSectors;
+	uint32_t largeSectors;
+	uint8_t physicalDiskNumber;
+	uint8_t currentHead;
+	uint8_t signature;
+	uint32_t volumeSerialNumber;
 	uint8_t volumeLabel[VOLUME_LABEL];
 	uint8_t systemID[SYSTEM_ID];
 
 	// Concluding fields
 	uint8_t bootstrap[BOOTSTRAP];
-	uint8_t end[END];
+	uint16_t end;
 
-} BootSector;
+}__attribute__((packed)) BootSector;
 
 
 #endif
